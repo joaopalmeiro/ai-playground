dfa711c935242db75b45888f9228e10e6d0f4880
João Palmeiro <joaommpalmeiro@gmail.com>
2026-07-23 16:35:35 +0100

    Add model

--- ipma-mcp/NOTES.md
+++ ipma-mcp/NOTES.md
@@ -5,9 +5,13 @@
 - https://py.sdk.modelcontextprotocol.io/v2/get-started/first-steps/#host-client-and-server
 - https://api.ipma.pt/public-data/forecast/locations.json
 - https://api.ipma.pt/public-data/forecast/aggregate/1110600.json
+- https://api.ipma.pt/open-data/forecast/meteorology/cities/daily/1110600.json
+- https://api.ipma.pt/
 - https://docs.langchain.com/oss/python/langchain/quickstart#build-a-real-world-agent
 - https://py.sdk.modelcontextprotocol.io/v2/servers/tools/#richer-schemas-with-field
 - https://py.sdk.modelcontextprotocol.io/v2/servers/tools/#async-def
 - https://py.sdk.modelcontextprotocol.io/v2/get-started/real-host/#vs-code
 - https://kiro.dev/docs/mcp/configuration/
 - https://github.com/modelcontextprotocol/python-sdk/blob/v2.0.0b1/examples/mcpserver/weather_structured.py
+- https://pydantic.dev/docs/validation/dev/api/pydantic/config/#pydantic.config.ConfigDict.alias_generator
+  - https://pydantic.dev/docs/validation/dev/api/pydantic/config/#pydantic.alias_generators.to_camel
--- ipma-mcp/server.py
+++ ipma-mcp/server.py
@@ -1,9 +1,41 @@
+from datetime import date, datetime
+from typing import Literal
+
+import httpx2
 from mcp.server import MCPServer
+from pydantic import BaseModel, ConfigDict
+from pydantic.alias_generators import to_camel
 
 mcp = MCPServer("IPMA Weather Forecast")
 
 
+class DailyForecast(BaseModel):
+    model_config = ConfigDict(alias_generator=to_camel)
+
+    precipita_prob: float
+    t_min: float
+    t_max: float
+    pred_wind_dir: Literal["NW", "N"]
+    id_weather_type: int
+    class_wind_speed: int
+    longitude: float
+    forecast_date: date
+    latitude: float
+
+
+class ForecastResponse(BaseModel):
+    model_config = ConfigDict(alias_generator=to_camel)
+
+    owner: Literal["IPMA"]
+    country: Literal["PT"]
+    global_id_local: int
+    data_update: datetime
+    data: list[DailyForecast]
+
+
 @mcp.tool()
-def add(a: int, b: int) -> int:
-    """Add two numbers."""
-    return a + b
+async def get_weather_forecast(city):
+    """TODO"""
+    async with httpx2.AsyncClient() as client:
+        response = await client.get("https://www.example.com/")
+        print(response)
